6c19a0c66fb8f85ec56bc9d45ae33f40ae727502
fk2019 <fk2019@gmail.com>
2023-09-13 19:20:08 +0300

    Add List typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Personal data methods"""
+from typing import List
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Obfuscate specific fields that match '=' plus other chars and separator
     using re.sub()"""
