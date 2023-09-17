7b69c166a6a5c0911abd32b1d9160b459068b5b8
fk2019 <fk2019@gmail.com>
2023-09-17 18:44:41 +0300

    Add forbidden error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unathorized() -> str:
       - raise 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raise 403 error
+    """
+    abort(403)
