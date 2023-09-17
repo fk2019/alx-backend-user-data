4237eb6404043a348b5bc74f62d9c75221218974
fk2019 <fk2019@gmail.com>
2023-09-17 17:05:23 +0300

    add encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Encrypt user passwords"""
+import bcrypt
+
+
+def hash_password(password):
+    """Return a hashed password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
