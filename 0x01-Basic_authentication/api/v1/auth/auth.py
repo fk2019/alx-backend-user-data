26ed2900aa3c71b0b84061acafaa552f79ec2b34
fk2019 <fk2019@gmail.com>
2023-09-23 10:13:21 +0300

    Add projects

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,11 +8,21 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return False"""
-        return False
+        if path is None or excluded_paths == []:
+            return True
+        for ex_path in excluded_paths:
+            if ex_path in {path, path + '/'}:
+                return False
+            else:
+                return True
 
     def authorization_header(self, request=None) -> str:
         """Return None"""
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return None"""
