0aa494ec4f81f0de4b42f59ed885cb39acb7dae7
fk2019 <fk2019@gmail.com>
2023-09-24 19:45:46 +0300

    Add app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+"""Flask app """
+from flask import Flask, jsonify, request,  abort, redirect, url_for
+from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /status
+    Return:
+      - JSON
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> str:
+    """ POST /users
+    Return:
+      - JSON payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueRrror:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+    Return:
+      - JSON payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    valid_user = AUTH.valid_login(email, password)
+    if not valid_user:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    message = {"email": email, "message": "logged in"}
+    response = jsonify(message)
+    response.set_cookie("session_id", session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /sessions
+    Return:
+      - Redirects user to status route (GET /)
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user_cookie is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ GET /profile
+    Return:
+        - user email.
+        - 403 for invalid session ID
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    if user_cookie is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+            - email
+        Return:
+            - Generate a Token
+            - 403 if email not registered
+    """
+    user_request = request.form
+    user_email = user_request.get('email')
+    is_registered = AUTH.create_session(user_email)
+    if not is_registered:
+        abort(403)
+    token = AUTH.get_reset_password_token(user_email)
+    message = {"email": user_email, "reset_token": token}
+    return jsonify(message)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ PUT /reset_password
+        Return:
+            - 403 if token is invalid
+    """
+    user_email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+    message = {"email": user_email, "message": "Password updated"}
+    return jsonify(message), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
