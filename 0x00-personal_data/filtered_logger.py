9dfe388ee7a73950bdb11c82d12fb4962262985f
fk2019 <fk2019@gmail.com>
2023-11-26 14:06:05 +0300

    Add filter datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,93 +1,81 @@
 #!/usr/bin/env python3
-"""Personal data methods"""
+""" Filter datum
+"""
+import re
+import os
+import mysql.connector
 from typing import List
 import logging
-import mysql.connector
-import os
-import re
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """Obfuscate specific fields that match '=' plus other chars and separator
-    using re.sub()"""
-    temp = message
-    for field in fields:
-        temp = re.sub(field + "=.*?" + separator, field + "=" + redaction +
-                      separator, temp)
-    return temp
+    """Filter datta"""
+    for entry in fields:
+        message = re.sub(rf"{entry}=(.*?)\{separator}",
+                         f'{entry}={redaction}{separator}', message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """Initilaize class"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ Initiliaze class """
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """filter values in incoming logs using filter_datum"""
+        """ Filter incoming logs values """
         return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
+                            super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """Return a Logger object"""
+    """ return logging.Logger object """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the db"""
-    db_host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_user = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
-    db_pass = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
-    db = os.environ.get('PERSONAL_DATA_DB_NAME')
-    if not (db_host and db_user and db_pass and db):
-        raise ValueError('Database credentials not found')
-    try:
-        conn = mysql.connector.connect(
-            host=db_host,
-            user=db_user,
-            password=db_pass,
-            database=db
-        )
-        return conn
-    except mysql.connector.Error as err:
-        print('Error: {}', err)
-        return None
+    """ Implement db conectivity"""
+    pass = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=pass)
+    return conn
 
 
 def main() -> None:
-    """main function"""
+    """ entry point"""
     db = get_db()
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM users')
-    logger = get_logger()
-    fld_a = 'name={}; email={}; phone={}; ssn={}; password={}; ip={};'
-    fld_b = 'last_login={}; user_agent={};'
+    cursor.execute("SELECT * FROM users;")
     for row in cursor:
-        fields = (fld_a + fld_b).format(row[0], row[1], row[2], row[3],
-                                        row[4], row[5], row[6], row[7])
-        logger.info(fields)
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
